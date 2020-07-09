7ec78425035f9981bb7d44662453635870d72002
Murilo Henrique <murilohns@gmail.com>
2020-07-08 22:18:09 -0300

    load category in find using eager

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -16,10 +16,7 @@ const upload = multer(uploadConfig);
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
 
-  const transactions = await transactionsRepository.find({
-    select: ['id', 'title', 'value', 'type', 'created_at', 'updated_at'],
-    relations: ['category'],
-  });
+  const transactions = await transactionsRepository.find();
 
   const balance = await transactionsRepository.getBalance();
 
@@ -37,8 +34,6 @@ transactionsRouter.post('/', async (request, response) => {
     type,
     category,
   });
-
-  delete transaction.category_id;
 
   return response.json(transaction);
 });
